67d80c8aca6f565360650ae1459f4f2b6de6cdad
SongKro <songkro48@gmail.com>
2020-04-18 18:01:02 +0700

    done

--- indexaccessing.js
+++ indexaccessing.js
@@ -3,7 +3,7 @@ var exampleFirstWord = word[0] + word[1] + word[2];
 var secondWord = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13] ;
 var thirdWord = word[15] + word[16];
 var fourthWord = word[18] + word[19];
-var fifthWord = word[21] + word[22] + word[23] + word[24]; // do your own!
+var fifthWord = word[21] + word[22] + word[23] + word[24];
 
 console.log('First Word: '  + exampleFirstWord);
 console.log('Second Word: ' + secondWord);
