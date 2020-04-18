8038b00fcf272442e049a1e4bf25a4c2da9dc315
SongKro <songkro48@gmail.com>
2020-04-18 15:43:29 +0700

    :rocket:

--- formasentence.js
+++ formasentence.js
@@ -5,5 +5,5 @@ var fourth = 'and';
 var fifth = 'I';
 var sixth = 'love';
 var seventh = 'it!';
-
+ 
 console.log(word + ' ' + second + ' '+ third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh)
