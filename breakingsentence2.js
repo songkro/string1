124c9e9568d2ef0b8e0c2936354bde2d4bdd647a
SongKro <songkro48@gmail.com>
2020-04-18 17:40:57 +0700

    edit1

---
+++ breakingsentence2.js
@@ -0,0 +1,12 @@
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3; 
+var thirdWord3;
+var fourthWord3;
+var fifthWord3;
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
